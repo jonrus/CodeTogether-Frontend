2823c2c3b3ee0c17011ffe0b7b6b8169a6355dee
Jon Russell <jon@jonrussell.xyz>
2021-05-23 14:20:35 -0500

    navbar

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,5 +1,6 @@
 import {Navbar, Nav, NavItem} from "reactstrap";
 import {NavLink} from "react-router-dom";
+import "./css/NavBar.css"
 
 interface NavBarProps {
     loggedIn: boolean
@@ -7,30 +8,38 @@ interface NavBarProps {
 
 export default function NavBar({loggedIn}: NavBarProps) {
     return (
-        <Navbar expand="md">
-            <NavLink exact to="/" className="navbar-brand">
-                Code Together!
-            </NavLink>
-            <Nav className="ml-auto" navbar>
-                <NavItem>
-                    <NavLink exact to="/about">About</NavLink>
-                </NavItem>
-                {!loggedIn && (
+        <div className="NavBar">
+            <Navbar expand="md">
+                <NavLink exact to="/" className="navbar-brand">
+                    Code Together!
+                </NavLink>
+                <Nav className="ml-auto" navbar>
                     <NavItem>
-                        <NavLink exact to="/signup">Sign Up</NavLink>
-                    </NavItem>)}
-                {!loggedIn && (
+                        <NavLink exact to="/about">About</NavLink>
+                    </NavItem>
+                    {!loggedIn && (
+                        <NavItem>
+                            <NavLink exact to="/signup">Sign Up</NavLink>
+                        </NavItem>)}
+                    {!loggedIn && (
+                        <NavItem>
+                            <NavLink exact to="/signin">Sign In</NavLink>
+                        </NavItem>)}
                     <NavItem>
-                        <NavLink exact to="/signin">Sign In</NavLink>
-                    </NavItem>)}
-                <NavItem>
-                    <NavLink exact to="/join-room">Join Room</NavLink>
-                </NavItem>
-                {loggedIn && (
-                    <NavItem>
-                        <NavLink exact to="/create-room">Create Room</NavLink>
-                    </NavItem>)}
-            </Nav>
-        </Navbar>
+                        <NavLink exact to="/join-room">Join Room</NavLink>
+                    </NavItem>
+                    {loggedIn && (
+                        <>
+                        <NavItem>
+                            <NavLink exact to="/create-room">Create Room</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink exact to="/logout">Logout</NavLink>
+                        </NavItem>
+                        </>)
+                    }
+                </Nav>
+            </Navbar>
+        </div>
     );
 }
